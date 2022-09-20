32ccd1141819e5f6846554bbea8f2f12501a4ce5
Claudio Alves Junior <claudio.alves@monitoratec.com.br>
2022-09-20 15:12:28 -0300

    github users domain

---
+++ lib/modules/user/domain/entities/user_entity.dart
@@ -0,0 +1,11 @@
+class UserEntity {
+  final String name;
+  final String id;
+  final String avatar;
+
+  UserEntity({
+    required this.name,
+    required this.id,
+    required this.avatar,
+  });
+}
