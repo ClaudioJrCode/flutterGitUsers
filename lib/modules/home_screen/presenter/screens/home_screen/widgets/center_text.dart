be1fff675b007a6789a236723f7410a60667e983
Claudio Alves Junior <claudio.alves@monitoratec.com.br>
2022-09-23 13:13:23 -0300

    Widget generico para texto centralizado

---
+++ lib/modules/home_screen/presenter/screens/home_screen/widgets/center_text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CenterText extends StatelessWidget {
+  final String text;
+  const CenterText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
