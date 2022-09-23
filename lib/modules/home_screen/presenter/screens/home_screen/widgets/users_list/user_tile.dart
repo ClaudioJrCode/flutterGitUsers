fe81de4e7b99088a78072dd473f67d807c4af7d9
Claudio Alves Junior <claudio.alves@monitoratec.com.br>
2022-09-23 13:14:13 -0300

    Widgets da lista de usuário

---
+++ lib/modules/home_screen/presenter/screens/home_screen/widgets/users_list/user_tile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:git_users/modules/home_screen/domain/entities/user_entity.dart';
+
+class UserTile extends StatelessWidget {
+  final UserEntity user;
+  const UserTile({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: Image.network(
+            user.avatar,
+          ),
+        ),
+        title: Text(user.name));
+  }
+}
---
+++ lib/modules/home_screen/presenter/screens/home_screen/widgets/users_list/users_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:git_users/modules/home_screen/domain/entities/user_entity.dart';
+import 'package:git_users/modules/home_screen/presenter/screens/home_screen/widgets/users_list/user_tile.dart';
+
+import 'custom_divider.dart';
+
+class UsersList extends StatelessWidget {
+  final List<UserEntity> users;
+  const UsersList({Key? key, required this.users}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: ListView.separated(
+          itemCount: users.length,
+          itemBuilder: (context, int index) => UserTile(user: users[index]),
+          separatorBuilder: (_, __) => const CustomDivider(),
+        ),
+      ),
+    );
+  }
+}
