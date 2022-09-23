ad6884a969c54526e9b318ff71acf7ba0b9decc4
Claudio Alves Junior <claudio.alves@monitoratec.com.br>
2022-09-23 13:13:37 -0300

    Barra de busca

---
+++ lib/modules/home_screen/presenter/screens/home_screen/widgets/search_bar/search_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const String hint = "Buscar usuários";
+
+class SearchBar extends StatelessWidget {
+  final Function(String) onSubmitted;
+  final Stream<String> stream;
+  final Function(String) onChaged;
+  final TextEditingController textController;
+
+  const SearchBar(
+      {super.key,
+      required this.onSubmitted,
+      required this.stream,
+      required this.onChaged,
+      required this.textController});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Object>(
+        stream: stream,
+        builder: (context, snapshot) {
+          return TextField(
+            controller: textController,
+            onChanged: onChaged,
+            decoration: const InputDecoration(
+              suffixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(),
+              hintText: hint,
+            ),
+            autocorrect: false,
+            onSubmitted: onSubmitted,
+          );
+        });
+  }
+}
