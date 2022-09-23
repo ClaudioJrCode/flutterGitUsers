f0e872c70d1326ace4c87078a39ddf9e99b8e25a
Claudio Alves Junior <claudio.alves@monitoratec.com.br>
2022-09-23 13:06:21 -0300

    renomeando arquivos e remoção de desnecessários

--- lib/modules/user/domain/repositories/user_repository_interface.dart
+++
@@ -1,7 +0,0 @@
-import 'package:git_users/core/exceptions/exception.dart';
-import 'package:git_users/modules/user/domain/entities/user_entity.dart';
-import '../../../../core/result.dart';
-
-abstract class IUserRepository {
-  Future<Result<Failure, List<UserEntity>>> getUsers({required String search});
-}
--- lib/modules/user/infra/datasource/user_data_source.dart
+++
@@ -1,5 +0,0 @@
-import '../../domain/entities/user_entity.dart';
-
-abstract class IUserDataSource {
-  Future<List<UserEntity>> getUsers({required String search});
-}
